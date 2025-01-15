9d4af1f0768262863a171d6e7d1c4ba6bbf8fef6
tanellelavallee <tanellelavallee@gmail.com>
2025-01-15 13:19:45 -0600

    Set up swagger in api.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
 import express, { Express } from "express";
 
+import setupSwagger from "../config/swagger";
+
 // Initialize Express application
 const app: Express = express();
+
+// setup swagger for api documentation
+setupSwagger(app);
+
+// set up swagger 
+/**
+ * @openapi
+ * /tasks:
+ *   get:
+ *     summary: Retrieve a list of tasks
+ *     tags: [Tasks]
+ *     responses:
+ *       200:
+ *         description: A list of tasks
+ */
+app.get("/tasks", (req, res) => {
+	res.send("Retrieve tasks");
+});
 
 // Define a route
 app.get("/", (req, res) => {
