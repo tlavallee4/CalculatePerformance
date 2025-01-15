d4b876949978f8fa4f2412c9f9fe7e42464f42a8
tanellelavallee <tanellelavallee@gmail.com>
2025-01-15 14:04:34 -0600

    Added testing

---
+++ test/app.test.ts
@@ -0,0 +1,11 @@
+import request from "supertest";
+import { Response } from "supertest";
+import app from "../src/app";
+
+describe("GET /", () => {
+	it("should return Simple Express app", async () => {
+		const response: Response = await request(app).get("/");
+		expect(response.status).toBe(200);
+		expect(response.text).toBe("Simple Express app");
+	});
+});
