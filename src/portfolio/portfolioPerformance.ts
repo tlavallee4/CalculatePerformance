1af93effe100a86c61f83290ffd15be5cdfcbc4f
tanellelavallee <tanellelavallee@gmail.com>
2025-01-17 14:14:04 -0600

    Added an array of conditions and outcomes instead of if statements

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -21,13 +21,59 @@ export function calculatePortfolioPerformance(initialInvestment: number, current
     // divide that by initial investment and * 100 to get the percentage
     const percentageChange = (profitOrLoss / initialInvestment) * 100;
 
+    // removing if statements 
+    // using condition & outcome instead
+    // using boolean (condition is met or not changing the outcome)
+    const performanceSummaryOutcomes = [
+        {
+            condition: function (percentageChange: number): boolean {
+                return percentageChange > 20;
+            },
+            // outcome is based on percentage and shows profit to the 2nd decimal place
+            outcome: `The portfolio has gained significantly with a profit of ${profitOrLoss.toFixed(2)}`
+        },
+        {
+            condition: function (percentageChange: number): boolean {
+                return percentageChange > 10 && percentageChange <= 20;
+            },
+            outcome: `The portfolio has gained moderately with a profit of ${profitOrLoss.toFixed(2)}`,
+        },
+        {
+            condition: function (percentageChange: number): boolean {
+                return percentageChange > 0 && percentageChange <= 10;
+            },
+            outcome: `The portfolio has gained slightly with a profit of ${profitOrLoss.toFixed(2)}`,
+        },
+        {
+            condition: function (percentageChange: number): boolean {
+                return percentageChange === 0;
+            },
+            outcome: "The portfolio has no change.",
+        },
+        {
+            condition: function (percentageChange: number): boolean {
+                return percentageChange < 0 && percentageChange >= -10;
+            },
+            outcome: `The portfolio has lost slightly with a loss of ${profitOrLoss.toFixed(2)}`,
+        },
+        {
+            condition:function (percentageChange: number): boolean {
+                return percentageChange < -10 && percentageChange >= -20;
+            },
+            outcome: `The portfolio has lost moderately with a loss of ${profitOrLoss.toFixed(2)}`,
+        },
+        {
+            condition: function (percentageChange: number): boolean {
+                return percentageChange < -20;
+            },
+            outcome: `The portfolio has lost significantly with a loss of ${profitOrLoss.toFixed(2)}`,
+        },
+    ];
 
-	let performanceSummary;
-	if (percentageChange > 20) {
-		performanceSummary = `The portfolio has gained significantly with a profit of $${profitOrLoss}.`;
-	} else {
-		performanceSummary = `The portfolio has performed poorly.`;
-	}
+    // need to choose the actual performance summary 
+    
+
+
 
 	return {
 		initialInvestment,
