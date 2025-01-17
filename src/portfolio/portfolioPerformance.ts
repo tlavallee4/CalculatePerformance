cd636b6fed043f61b5b7c506cad135f754794e64
tanellelavallee <tanellelavallee@gmail.com>
2025-01-17 13:22:00 -0600

    Added a typescript interface, changed the set numbers to inputs

--- src/portfolio/portfolioPerformance.ts
+++ src/portfolio/portfolioPerformance.ts
@@ -1,10 +1,26 @@
-export function calculatePortfolioPerformance(): any {
-	let initialInvestment = 10000;
-	let currentValue = 12000;
+// create an interface for typescript
+interface PortfolioPerformance {
+    initialInvestment : number;
+    currentValue: number;
+    profitOrLoss: number;
+    percentageChange: number; 
+    performanceSummary: string;
+}
 
-	const profitOrLoss = initialInvestment / currentValue;
 
-	const percentageChange = (profitOrLoss / initialInvestment) * 100;
+export function calculatePortfolioPerformance(initialInvestment: number, currentValue: number): PortfolioPerformance {
+// change initial investment and current value to be inputs instead of set numbers
+// call the typescript interface
+
+    // you need to subtract the current value from what was initially invested
+    // to find if there is an actual gain or loss
+    const profitOrLoss = currentValue - initialInvestment;
+
+    // change to current value subtract initial investment
+    // this is the amount of profit or loss
+    // divide that by initial investment and * 100 to get the percentage
+    const percentageChange = (profitOrLoss / initialInvestment) * 100;
+
 
 	let performanceSummary;
 	if (percentageChange > 20) {
